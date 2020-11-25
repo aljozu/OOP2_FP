276732afd4102f883234044f874c46287277c478
Alex Jose Loja Zumaeta <alex.loja@utec.edu.pe>
2020-11-25 13:10:47 -0500

    cmake fixed

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -59,5 +59,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/plsdiosito.dir/DependInfo.cmake"
+  "CMakeFiles/OOP2_FP.dir/DependInfo.cmake"
   )
